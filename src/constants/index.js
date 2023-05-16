2567e2b420fcc38f612fb3c192d978c6bfc26a4d
sardordev20 <uroqovsardor510@gmail.com>
2023-05-16 08:14:20 +0500

    category component end

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,32 @@
-export const logo = '/img/logo_black.png';
+import {
+  Checkroom,
+  DeveloperMode,
+  FaceRetouchingNatural,
+  FitnessCenter,
+  GraphicEq,
+  Home,
+  LiveTv,
+  MusicNote,
+  OndemandVideo,
+  School,
+  SportsEsports,
+  TheaterComedy,
+} from "@mui/icons-material";
+
+export const logo = "/img/logo_black.png";
+
+export const category = [
+  { name: "New", icon: <Home /> },
+  { name: "Movie", icon: <OndemandVideo /> },
+  { name: "Live", icon: <LiveTv /> },
+  { name: "Gaming", icon: <SportsEsports /> },
+  { name: "Education", icon: <School /> },
+  { name: "Sport", icon: <FitnessCenter /> },
+  { name: "Comedy", icon: <TheaterComedy /> },
+  { name: "Podcast", icon: <GraphicEq /> },
+  { name: "Fashion", icon: <Checkroom /> },
+  { name: "Crypto", icon: <DeveloperMode /> },
+  { name: "Gym", icon: <FitnessCenter /> },
+  { name: "Beauty", icon: <FaceRetouchingNatural /> },
+  { name: "Music", icon: <MusicNote /> },
+];
